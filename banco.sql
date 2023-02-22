4e31d9e13067cdc9d13eef9201387bf24bc7dab8
Gabriel Amed Mlaker da Silva <mlakergabriel@gmail.com>
2023-02-22 16:46:57 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,5 +1,14 @@
-CREATE TABLE dados (
-    AlunoID int,
+CREATE TABLE cliente (
+    ClienteID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
+
+CREATE TABLE funcionario (
+    funcionarioID int,
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
